14fc49583ddd6c89d5354ff86d6f2761781ac293
Lotario72 <nikvel250@gmail.com>
2022-08-30 21:05:02 -0500

    Update Data_cleaning.R

--- scripts/Data_cleaning.R
+++ scripts/Data_cleaning.R
@@ -1,3 +1,4 @@
+#################################Punto 1########################################
 # Cleaning Data
 rm(list = ls())
 
@@ -19,7 +20,7 @@ p_load(  rio, # import/export data
          broom, ## tidy function
          broom.mixed,
          skimr,
-         stargazer,sandwich, kable, kableExtra,ggplot2,GGally, ggcorrplot,stargazer, xtable, hrbrthemes)
+         stargazer,sandwich, kable, kableExtra,ggplot2,GGally, ggcorrplot,stargazer, xtable, hrbrthemes,boot)
 
 ## Import dataset
 
@@ -31,6 +32,17 @@ df <- df[df$age >=18,]
 
 data <- df %>% select(!matches(c(("^p[0-9]"), "^cc", "^io", "^y_", "^fex", "^hours"))) %>% select(-(ina:ingtotes), -c(depto,dominio ,clase, V1, wap, directorio, secuencia_p, pet, orden, mes, fweight, informal, cuentaPropia, pea, microEmpresa)) %>% replace_na(list(oficio = 0, relab = 0, totalHoursWorked = 0)) 
 
+y_salary_m = df$y_salary_m
+
+y_ingLab_m = df$y_ingLab_m
+
+y_total_m =  df$y_total_m
+
+data$y_ingLab_m = y_ingLab_m
+
+data$y_salary_m = y_salary_m
+
+data$y_total_m = y_total_m
 ## Descriptives
 skim(data)
 
@@ -56,3 +68,26 @@ ggplot(data, aes(x=ingtot))+ geom_histogram()+
   theme_ipsum() 
 
 ##Boxplot by Gender
+
+box_plot <- ggplot(data = data, mapping = aes(as.factor(sex), ingtot)) +
+  geom_boxplot()
+
+box_plot + ggtitle("Box plot") + theme(plot.title = element_text(hjust = 0.5))
+
+
+
+#################################Punto 2########################################
+
+mod_age <- lm("ingtot ~ age + I(age^2)" , data= data)
+
+summary(mod_age)
+results = tidy(mod_age)
+stargazer(mod_age, dep.var.labels=c("Earnings") , out="./views/Modelo_Age.tex")
+
+boot(data = data, statistic = corr, R = 1000)
+
+
+data2 <-df %>% select(matches("y_")) %>% colSums(na.rm=TRUE)
+
+summar(data2, type = 'text')
+data2
