0495fb8d2bb7b5cdde6578ee838f42900ac46df3
Lotario72 <nikvel250@gmail.com>
2022-09-02 11:15:54 -0500

    Update Data_cleaning.R
    Se actualizó el filtro para empleados y se remplazó con la moda para los datos faltantes en las categoricas de pensiones y  Regimen de Salud

--- scripts/Data_cleaning.R
+++ scripts/Data_cleaning.R
@@ -20,7 +20,7 @@ p_load(  rio, # import/export data
          broom, ## tidy function
          broom.mixed,
          skimr,
-         stargazer,sandwich, kable, kableExtra,ggplot2,GGally, ggcorrplot,stargazer, xtable, hrbrthemes,boot)
+         stargazer,sandwich, kable, kableExtra,ggplot2,GGally, ggcorrplot,stargazer, xtable, hrbrthemes,boot, dplyr)
 
 ## Import dataset
 
@@ -29,8 +29,9 @@ df <- import(here("./stores/data.csv"))
 ## Filter by age >18 & Cleaning Data set
 ##NOTE: Confirm if this should be '>' or '>=' 
 df <- df[df$age >=18,]
+df <- df[df$ocu ==1,]
 
-data <- df %>% select(!matches(c(("^p[0-9]"), "^cc", "^io", "^y_", "^fex", "^hours"))) %>% select(-(ina:ingtotes), -c(depto,dominio ,clase, V1, wap, directorio, secuencia_p, pet, orden, mes, fweight, informal, cuentaPropia, pea, microEmpresa)) %>% replace_na(list(oficio = 0, relab = 0, totalHoursWorked = 0)) 
+data <- df %>% select(!matches(c(("^p[0-9]"), "^cc", "^io", "^y_", "^fex", "^hours"))) %>% select(-(ina:ingtotes), -c(depto,dominio ,clase, V1, wap, directorio, secuencia_p, pet, orden, mes, fweight, informal, cuentaPropia, pea, microEmpresa, ocu, dsi, inac)) %>% replace_na(list(oficio = 0, relab = 0, totalHoursWorked = 0)) 
 
 y_salary_m = df$y_salary_m
 
@@ -43,6 +44,22 @@ data$y_ingLab_m = y_ingLab_m
 data$y_salary_m = y_salary_m
 
 data$y_total_m = y_total_m
+
+categoricas <- c('estrato1','oficio','relab','maxEducLevel','regSalud','cotPension')
+for (v in categoricas) {
+  data[,v] <- as.factor(data[,v,drop=T])
+} 
+
+##Missing Values in categorical replacing with mode
+
+table(data$maxEducLevel)
+
+data$maxEducLevel[is.na(data$maxEducLevel)]<-7
+
+table(data$regSalud)
+
+data$regSalud[is.na(data$regSalud)]<-1
+
 ## Descriptives
 skim(data)
 
@@ -51,8 +68,9 @@ stargazer(data)
 # Check correlations (as scatterplots), distribution and print corrleation coefficient
 
 #db <- as_tibble(df) ## from dataframe to tibble
+#data2 <-df %>% select(matches("y_")) %>% colSums(na.rm=TRUE)
 
-data_plot <- data %>% select(-c('regSalud','cotPension','formal','sizeFirm','maxEducLevel'))
+data_plot <- data %>% select(-c('estrato1','oficio','relab','maxEducLevel','regSalud','cotPension'))
 
 corr <- round(cor(data_plot), 1)
 
@@ -63,16 +81,15 @@ ggcorrplot(corr, method = 'circle', type = 'lower', lab = TRUE) +
 
 ##Histogram Ingreso
 
-ggplot(data, aes(x=ingtot))+ geom_histogram()+
-  ggtitle("Ingresos Totales") +
-  theme_ipsum() 
+ggplot(data, aes(x=ingtot))+ geom_histogram(bins=20, fill = "darkorange")+
+  ggtitle("Ingresos Totales") + labs(x="Ingreso Total", y = "Cantidad")+
+  theme_bw() 
 
-##Boxplot by Gender
+##Bar chart for categorical variables
 
-box_plot <- ggplot(data = data, mapping = aes(as.factor(sex), ingtot)) +
-  geom_boxplot()
-
-box_plot + ggtitle("Box plot") + theme(plot.title = element_text(hjust = 0.5))
+ggplot(data, aes(x=estrato1))+ geom_histogram(bins=20, fill = "darkorange")+
+  ggtitle("Estrato") + labs(x="Estrato", y = "Cantidad")+
+  theme_bw() 
 
 
 
@@ -84,10 +101,22 @@ summary(mod_age)
 results = tidy(mod_age)
 stargazer(mod_age, dep.var.labels=c("Earnings") , out="./views/Modelo_Age.tex")
 
-boot(data = data, statistic = corr, R = 1000)
 
+beta.fn<-function(data,index){
+  coef(lm(ingtot ~ age + I(age^2) , data = data, subset = index))
+}
 
-data2 <-df %>% select(matches("y_")) %>% colSums(na.rm=TRUE)
+plotbot <- boot(data, beta.fn, R = 1000)
 
-summar(data2, type = 'text')
-data2
+plot(plotbot)
+
+boot.ci(plotbot, type="bca")
+
+#plot predicted vs. actual values
+
+ggplot(data, aes(y=predict(mod_age), x= age)) + 
+  labs(x='Age', y='Predicted Values', title='Predicted Values vs. Age')+ geom_point() 
+
+  
+  
+  
