e457e065f9341a1fbc688d5f046160e1af71a348
Camilo Andrés Bonilla Hernández <ca_m_ilo@hotmail.com>
2022-09-05 18:20:24 -0500

    Fnishing touches

--- scripts/performance_tests.R
+++ scripts/performance_tests.R
@@ -1,7 +1,8 @@
-source("./scripts/data_cleaning.R")
+source("../scripts/data_cleaning.R")
 library("caret")
 library("doSNOW")
 library("foreach")
+library("stargazer")
 
 
 # create ID column
@@ -15,12 +16,13 @@ train <- dplyr::select(train, -id)
 test <- dplyr::select(test, -id)
 
 # Solve issue when fitting a regression
-test <- test %>% semi_join(train, by = "relab")
+test <- test %>% semi_join(train, by = c("oficio", "relab"))
 
+# Generate formulas to be used in the model comparison
 formulas <- c(
     "age + I(age ^ 2)",
     "female*age + I(age ^ 2) + female:I(age ^ 2)",
-    "female + age + totalHoursWorked + sizeFirm + relab + maxEducLevel + oficio",
+    "female + age + totalHoursWorked + relab + sizeFirm + maxEducLevel + oficio",
     "poly(totalHoursWorked, 3)*sizeFirm + female",
     "poly(age, 3) + maxEducLevel",
     "poly(log(age), 3)*poly(log(totalHoursWorked), 3)"
@@ -29,21 +31,28 @@ formulas <- c(
 dependent <- "log(y_salary_m) ~ "
 formulas <- paste0(dependent, formulas)
 
-
+# Fit a model and compute RMSE with respect to the test set
 lm_fit <- function(formula) {
     fit_reg <- lm(formula, train)
-    preds <- predict(fit_reg, test)
-    rmse <- caret::RMSE(preds, test$ingtot, na.rm = TRUE)
+    preds <- predict(
+        fit_reg,
+        test
+    )
+    rmse <- caret::RMSE(preds, log(test$y_salary_m), na.rm = TRUE)
     rmse
 }
 
 errors <- numeric()
 
+# Use lm_fit with each specification in formulas
 for (i in formulas) {
     error <- lm_fit(i)
     errors[i] <- error
 }
 
+stargazer(errors, summary = FALSE, rownames = FALSE)
+
+# Select best models with regards to RMSE value (smallest)
 best_models <- function(n) {
     sorted <- sort(errors)
     best <- sorted[1:n]
@@ -53,6 +62,7 @@ best_models <- function(n) {
 cl <- makeCluster(4, type = "SOCK")
 registerDoSNOW(cl)
 
+# Compute influences for observations in test set (parallelized)
 for (i in best_models(1)) {
     influences <- foreach(j = 1:nrow(test), .combine = c) %dopar% {
         temp_set <- rbind(train, test[j, ])
@@ -63,13 +73,14 @@ for (i in best_models(1)) {
     }
 }
 
+# Summarize influences over train and test sets
 histogram(influences)
 boxplot(influences)
+boxplot(dffits(lm(best_models(1), train)))
 summary(influences)
 
+# Cross-validate third and fifth specifications with LOOCV
 train_control <- trainControl(method = "LOOCV")
-
-
 trainer <- function(formula) {
     caret::train(
         as.formula(formula),
@@ -80,11 +91,12 @@ trainer <- function(formula) {
     )
 }
 
-system.time({
-    model1 <- trainer(best_models(2)[1])
-    model2 <- trainer(best_models(2)[2])
-})
+
+model1 <- trainer(best_models(2)[1])
+model2 <- trainer(best_models(2)[2])
+
+# Stop parallel cluster
 stopCluster(cl)
 
-model1$results
-model2$results
+stargazer(model1$results, title = "Desempeño del modelo 3", summary = FALSE, rownames = FALSE)
+stargazer(model2$results, title = "Desempeño del modelo 5", summary = FALSE, rownames = FALSE)
